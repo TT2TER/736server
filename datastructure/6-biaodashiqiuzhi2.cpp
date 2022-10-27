786a6f64fdeb2c9b04f5107a74e0fe901cff3683
mayu <2270610334@qq.com>
2022-10-27 22:13:24 +0800

    gengxin

---
+++ datastructure/6-biaodashiqiuzhi2.cpp
@@ -0,0 +1,295 @@
+#include <cstdio>
+#include <iostream>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <map>
+
+using namespace std;
+struct Ret
+{
+    int num;
+    bool m; // n/y
+};
+map<char [10],int> var;
+stack<int> num;
+stack<char> sig;
+//一起用来定位优先顺序
+char operation[9] =
+    {'+', '-', '*', '/', '(', ')', '#', '^', '%'};
+char checklist[9][9] = {
+    {'>', '>', '<', '<', '<', '>', '>', '<', '<'},
+    {'>', '>', '<', '<', '<', '>', '>', '<', '<'},
+    {'>', '>', '>', '>', '<', '>', '>', '<', '>'},
+    {'>', '>', '>', '>', '<', '>', '>', '<', '>'},
+    {'<', '<', '<', '<', '<', '=', ' ', '<', '<'},
+    {'>', '>', '>', '>', ' ', '>', '>', '>', '>'},
+    {'<', '<', '<', '<', '<', ' ', '=', '<', '<'},
+    {'>', '>', '>', '>', '<', '>', '>', '<', '>'},
+    {'>', '>', '>', '>', '<', '>', '>', '<', '>'}};
+
+int char2num(char *q)
+{
+    return (int)(*q - '0');
+}
+
+char compare(char x, char y)
+{
+    int a, b; //用来定位下标输出优先顺序
+    for (int i = 0; i < 9; i++)
+    {
+        if (operation[i] == x)
+        {
+            a = i;
+            break;
+        }
+    }
+    for (int i = 0; i < 9; i++)
+    {
+        if (operation[i] == y)
+        {
+            b = i;
+            break;
+        }
+    }
+    //得到优先级关系并且作为返回值
+    return checklist[a][b];
+}
+
+Ret operate(int x, int y, char symbol)
+{
+    Ret ret;
+    switch (symbol)
+    {
+    case '+':
+        ret.num = x + y;
+        ret.m = true;
+        break;
+    case '-':
+        ret.num = x - y;
+        ret.m = true;
+        break;
+    case '*':
+        ret.num = x * y;
+        ret.m = true;
+        break;
+    case '/':
+        if (y)
+        {
+            ret.num = x / y;
+            ret.m = true;
+            break;
+        }
+        else
+        {
+            printf("Divide 0.\n");
+            ret.m = false;
+            break;
+        }
+    case '%':
+        ret.num = (int)fmod(x, y);
+        ret.m = true;
+        break;
+    case '^':
+        if (y >= 0)
+        {
+            ret.num = (int)pow(x, y);
+            ret.m = true;
+            break;
+        }
+        else
+        {
+            printf("error.\n");
+            ret.m = false;
+            break;
+        }
+    default:
+        printf("error.\n");
+        ret.m = false;
+        break;
+    }
+    return ret;
+}
+
+int main()
+{
+    
+    int flag; //数字0，（ 为1，其他2
+    char expr[100], *p, *nega;
+    char fnega = '0';
+
+    char g[100];//读进来整个字符串，然后处理字符串
+    scanf("%s", g);
+    
+
+    while (strcmp("end",g)!=0)
+    {
+        int option=1;//1为方程，2为问号，输出
+        char now[10];
+        now[0]='\0';
+        if(g[0]=='?')
+        option =2;
+        if (option==2)
+        {
+            int i;
+            for(i=0;i<10&&g[i+2]!='\0';i++){
+                now[i]=g[i+2];
+            }
+            now[i]='\0';
+        }
+        else if(option ==1)
+        {
+            int i;
+            for(i=0;i<10&&g[i]!='=';i++){
+                now[i]=g[i];
+            }
+            now[i]='\0';
+        }
+        while (!num.empty())
+        {
+            num.pop();
+        }
+        while (!sig.empty())
+        {
+            sig.pop();
+        }
+
+        flag = 2;
+        memset(expr, '\0', 100);
+        scanf("%s", expr);
+
+        strcat(expr, "#");
+        p = expr;
+        nega = expr;
+
+        // 第一个符号是负数号且第二个字符是数字
+        // 把负数号替换为0
+        if (*nega == '-' && *(nega + 1) == '(')
+        { //型如-(2+2)+1 直接在num.stack中插入一个0
+            num.push(char2num(&fnega));
+        }
+
+        if (*nega == '-' && *(nega + 1) >= '0' && *(nega + 1) <= '9')
+        {
+            *nega = '0';
+        }
+        nega++;
+        // 遍历每一个字符
+        for (; *nega != '\0'; nega++)
+        {
+            if ((*(nega - 1) < '0' || *(nega - 1) > '9') && (*nega == '-') && *(nega - 1) != ')')
+            {
+                *nega = '0';
+            }
+        }
+        sig.push('#');
+        int dead = 0;
+        while (*p != '#' || sig.top() != '#')
+        {
+            if (*p >= '0' && *p <= '9')
+            {
+                if (flag == 0)
+                {
+                    if (num.top() <= 0)
+                    {
+                        int q = num.top();
+                        num.pop();
+                        num.push((q * 10 + char2num(p++) * (-1)));
+                    }
+                    else
+                    {
+                        int q = num.top();
+                        num.pop();
+                        num.push(q * 10 + char2num(p++));
+                    }
+                }
+                else
+                    num.push(char2num(p++));
+                flag = 0;
+            }
+            else
+            {
+                //（+
+                if (flag == 1)
+                {
+                    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '%' || *p == '^')
+                    {
+                        printf("error.\n");
+                        {
+                            dead = 1;
+                            break;
+                        }
+                    }
+                }
+                //左括号标记为1
+                if (*p == '(')
+                {
+                    flag = 1;
+                }
+                else
+                    flag = 2;
+
+                if (sig.empty())
+                {
+                    printf("error.\n");
+                    {
+                        dead = 1;
+                        break;
+                    }
+                }
+                else
+                {
+                    char ch = sig.top(), ans;
+                    sig.pop();
+                    //优先级比较
+                    ans = compare(ch, *p);
+                    if (ans == ' ')
+                    {
+                        printf("error.\n");
+                        {
+                            dead = 1;
+                            break;
+                        }
+                    }
+                    else if (ans == '<')
+                    {
+                        sig.push(ch); 
+                        sig.push(*p++);
+                        continue;
+                    }
+                    
+                    else if (ans == '=')
+                    {
+                        p++;
+                        continue;
+                    }
+                    else
+                    {
+                        int integer_x, integer_y;
+                        integer_y = num.top();
+                        num.pop();
+                        integer_x = num.top();
+                        num.pop();
+                        Ret judge = operate(integer_x, integer_y, ch);
+                        if (judge.m == false)
+                        {
+                            dead = 1;
+                            break;
+                        }
+                        else
+                            num.push(judge.num);
+                        continue;
+                    }
+                    p++;
+                }
+            }
+        }
+        if (!dead && num.size() == 1 && sig.size() == 1)
+            printf("%d\n", num.top());
+        else if (!dead)
+        {
+            printf("error.\n");
+        }
+    }
+    return 0;
+}
---
+++ datastructure/tempCodeRunnerFile.cpp
@@ -0,0 +1,272 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+typedef struct order
+{
+    int orderid;
+    int stockid;
+    float price;
+    int quantity;
+    char type;
+} order;
+
+int min(int a, int b)
+{
+    return a <= b ? a : b;
+}
+
+typedef struct QNode
+{
+    order data;
+    struct QNode *link;
+} QNode, *QueuePtr;
+
+typedef struct
+{
+    QueuePtr front; // 队头指针
+    QueuePtr rear;  // 队尾指针
+} LinkQueue;
+
+void InitQueue(LinkQueue &Q)
+{
+    // 构造一个有头结点的空队列Q
+    Q.front = (QueuePtr)malloc(sizeof(QNode));
+    Q.rear = Q.front;
+    Q.front->link = NULL;
+    return;
+} // 出队
+
+void save(LinkQueue &t, QueuePtr p)
+{
+    QueuePtr q = t.front, q_pre;
+    int flag = 0;
+    while (q->link != NULL)
+    {
+        q_pre = q;
+        q = q->link;
+        if (p->data.stockid == q->data.stockid)
+        {
+            flag = 1;
+            if (p->data.type == 'b')
+            {
+                while (q != NULL && q->data.price >= p->data.price && q->data.stockid == p->data.stockid)
+                {
+                    q_pre = q;
+                    q = q->link;
+                }
+                q_pre->link = p;
+                p->link = q;
+                break;
+            }
+            else
+            {
+                while (q != NULL && q->data.price <= p->data.price && q->data.stockid == p->data.stockid)
+                {
+                    q_pre = q;
+                    q = q->link;
+                }
+                q_pre->link = p;
+                p->link = q;
+                break;
+            }
+        }
+    }
+    if (flag == 0)
+    {
+        p->link = t.front->link;
+        t.front->link = p;
+    }
+}
+
+void delorder(LinkQueue &t, QueuePtr p)
+{
+    QueuePtr x = t.front;
+    while (x->link->data.orderid != p->data.orderid)
+        x = x->link;
+    x->link = p->link;
+    free(p);
+}
+
+int allorderid = 0;
+
+int main()
+{
+    LinkQueue sell, buy;
+    InitQueue(buy);
+    InitQueue(sell);
+    int command = -1;
+    scanf("%d", &command);
+    while (command != 0)
+    {
+        if (command == 1)
+        {
+            int id, quantity;
+            float price;
+            char BS;
+            scanf("%d %f %d %c", &id, &price, &quantity, &BS);
+            order now;
+            now.orderid = ++allorderid;
+            now.price = price;
+            now.quantity = quantity;
+            now.stockid = id;
+            now.type = BS;
+            printf("orderid: %04d\n", now.orderid);
+            if (BS == 'b')
+            {
+                QueuePtr q = sell.front;
+                while (q->link != NULL)
+                {
+                    q = q->link;
+                    if (q->data.stockid != now.stockid)
+                    {
+                        continue;
+                    }
+                    else if (q->data.price <= now.price)
+                    {
+                        int dealquantity = min(now.quantity, q->data.quantity);
+                        printf("deal--price:%6.1f  quantity:%4d  buyorder:%04d  sellorder:%04d\n", (now.price + q->data.price) / 2.0, dealquantity, now.orderid, q->data.orderid);
+                        now.quantity -= dealquantity;
+                        q->data.quantity -= dealquantity;
+                        if (q->data.quantity == 0)
+                            delorder(sell, q);
+                        if (now.quantity == 0)
+                            break;
+                    }
+                    else
+                        continue;
+                }
+                if (now.quantity != 0)
+                {
+                    QueuePtr t = (QueuePtr)malloc(sizeof(QNode));
+                    t->data.orderid = now.orderid;
+                    t->data.price = now.price;
+                    t->data.quantity = now.quantity;
+                    t->data.stockid = now.stockid;
+                    t->data.type = now.type;
+                    t->link = NULL;
+                    save(buy, t);
+                }
+            }
+            else
+            {
+                QueuePtr q = buy.front;
+                while (q->link != NULL)
+                {
+                    q = q->link;
+                    if (q->data.stockid != now.stockid)
+                    {
+                        continue;
+                    }
+                    else if (q->data.price >= now.price)
+                    {
+                        int dealquantity = min(now.quantity, q->data.quantity);
+                        printf("deal--price:%6.1f  quantity:%4d  buyorder:%04d  sellorder:%04d\n", (now.price + q->data.price) / 2.0, dealquantity, now.orderid, q->data.orderid);
+                        now.quantity -= dealquantity;
+                        q->data.quantity -= dealquantity;
+                        if (q->data.quantity == 0)
+                            delorder(buy, q);
+                        if (now.quantity == 0)
+                            break;
+                    }
+                    else
+                        continue;
+                }
+                if (now.quantity != 0)
+                {
+                    QueuePtr t = (QueuePtr)malloc(sizeof(QNode));
+                    t->data.orderid = now.orderid;
+                    t->data.price = now.price;
+                    t->data.quantity = now.quantity;
+                    t->data.stockid = now.stockid;
+                    t->data.type = now.type;
+                    t->link = NULL;
+                    save(sell, t);
+                }
+            }
+            QueuePtr t = sell.front;
+            while (t->link != NULL)
+            {
+                t = t->link;
+                if (t->data.quantity == 0)
+                {
+                    delorder(sell, t);
+                }
+            }
+            t = buy.front;
+            while (t->link != NULL)
+            {
+                t = t->link;
+                if (t->data.quantity == 0)
+                {
+                    delorder(buy, t);
+                }
+            }
+        }
+        else if(command==2)
+        {
+            int checkstockid;
+            scanf("%d", &checkstockid);
+            getchar();
+            printf("buy orders:\n");
+            QueuePtr p = buy.front;
+            while (p->link != NULL)
+            {
+                p = p->link;
+                if (p->data.stockid == checkstockid && p->data.quantity != 0)
+                {
+                    printf("orderid: %04d, stockid:%04d, price: %6.1f, quantity: %4d, b/s: %c\n", p->data.orderid, p->data.stockid, p->data.price, p->data.quantity, p->data.type);
+                }
+            }
+            printf("sell orders:\n");
+            p = sell.front;
+            while (p->link != NULL)
+            while (p->link != NULL)
+            {
+                p = p->link;
+                if (p->data.stockid == checkstockid && p->data.quantity != 0)
+                {
+                    printf("orderid: %04d, stockid:%04d, price: %6.1f, quantity: %4d, b/s: %c\n", p->data.orderid, p->data.stockid, p->data.price, p->data.quantity, p->data.type);
+                }
+            }
+        }
+        else if(command==3)
+        {
+            int delorderid;
+            scanf("%d", &delorderid);
+            getchar();
+            int flag = 0;
+            QueuePtr p = buy.front;
+            while (p->link != NULL)
+            {
+                p = p->link;
+                if (p->data.orderid == delorderid)
+                {
+                    flag = 1;
+                    printf("deleted order:orderid: %04d, stockid:%04d, price: %6.1f, quantity: %4d, b/s: %c\n", p->data.orderid, p->data.stockid, p->data.price, p->data.quantity, p->data.type);
+                    delorder(buy,p);
+                    break;
+                }
+            }
+            p = sell.front;
+            while (p->link != NULL && !flag)
+            {
+                p = p->link;
+                if (p->data.orderid == delorderid)
+                {
+                    flag = 1;
+                    printf("deleted order:orderid: %04d, stockid:%04d, price: %6.1f, quantity: %4d, b/s: %c\n", p->data.orderid, p->data.stockid, p->data.price, p->data.quantity, p->data.type);
+                    delorder(sell, p);
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                printf("not found\n");
+            }
+            flag = 0;
+        }
+        scanf("%d", &command);
+    }
+    return 0;
+}
